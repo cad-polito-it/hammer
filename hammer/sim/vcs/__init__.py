b797d980280764afd9a9b9f713a79ba356c47d86
Fra <francesco.angione@polito.it>
2025-10-17 20:32:01 -0400

    feat: adding elaboration dir also for vcs run

--- hammer/sim/vcs/__init__.py
+++ hammer/sim/vcs/__init__.py
@@ -242,6 +242,8 @@ class VCS(HammerSimTool, SynopsysTool):
         if os.path.exists(os.path.join(self.run_dir, "csrc")):
             shutil.rmtree(os.path.join(self.run_dir, "csrc"))
 
+        # Adding output elaboration dir
+        args.append("-Mdir={}".format(self.run_dir))
         # Generate a simulator
         self.run_executable(args, cwd=self.run_dir)
 
