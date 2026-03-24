91a78083d3749dc4d87e9289e91c4527bf3edc7a
Francesco Angione <francesco.angione@polito.it>
2026-03-24 08:17:06 -0400

    feat: adding timing dependancies for fault simulation (for small delay faults)

--- hammer/vlsi/hammer_build_systems.py
+++ hammer/vlsi/hammer_build_systems.py
@@ -218,7 +218,7 @@ def build_makefile(driver: HammerDriver, append_error_func: Callable[[str], None
         {fsim_syn_in}: {syn_out}
         \t$(HAMMER_EXEC) {env_confs} -p {syn_out} $(HAMMER_EXTRA_ARGS) -o {fsim_syn_in} --obj_dir {obj_dir} syn-to-fsim
 
-        {fsim_syn_out}: {fsim_syn_in} $(HAMMER_SIM_SYN_DEPENDENCIES)
+        {fsim_syn_out}: {fsim_syn_in} $(HAMMER_SIM_SYN_DEPENDENCIES) $(HAMMER_FSIM_TIMING_DEPENDENCIES)
         \t$(HAMMER_EXEC) {env_confs} -p {fsim_syn_in} $(HAMMER_EXTRA_ARGS) --fsim_rundir {fsim_syn_run_dir} --obj_dir {obj_dir} fsim{suffix}
 
         {power_sim_syn_in}: {sim_syn_out}
